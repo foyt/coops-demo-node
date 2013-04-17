9fa3768e882257f1d1fa07c50eb21cbd84aee0c8
Antti Leppä <antti.leppa@foyt.fi>
2013-04-17 20:36:33 +0300

    ** Added database

---
+++ database.js
@@ -0,0 +1,48 @@
+(function() {
+
+  var mongoose = require('mongoose');
+  mongoose.connect('mongodb://localhost/coops-demo-node');
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  
+  /* User */
+  
+  var UserSchema = mongoose.Schema({
+    name: String,
+    userId: String,
+    accessToken: String,
+    refreshToken: String,
+    tokenExpires: Number
+  });
+  
+  /* UserEmail */
+  
+  var UserEmailSchema = mongoose.Schema({
+    userId: mongoose.Schema.Types.ObjectId,
+    email: String
+  });
+
+  /* UserIdentifier */
+  
+  var UserIdentifierSchema = mongoose.Schema({
+    userId: mongoose.Schema.Types.ObjectId,
+    identifier: String
+  });
+  
+  /* Setting */
+  
+  var SettingSchema = mongoose.Schema({
+    key: String,
+    value: String
+  });
+
+  module.exports = {
+    model: {
+      User: mongoose.model('User', UserSchema),
+      UserEmail: mongoose.model('UserEmail', UserEmailSchema),
+      UserIdentifier: mongoose.model('UserIdentifier', UserIdentifierSchema),
+      Setting: mongoose.model('Setting', SettingSchema) 
+    }
+  };
+  
+}).call(this);
