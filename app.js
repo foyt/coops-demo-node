7a16c3fd0d7a95c5ae0dc9712c40941a0ad8998f
Antti Leppä <antti.leppa@foyt.fi>
2013-04-17 20:37:18 +0300

    ** Added main application ** Added github and facebook authentication strategies

---
+++ app.js
@@ -0,0 +1,106 @@
+// http://openclipart.org/detail/169130/mapa-de-redes-by-ainara14-169130
+
+var http = require('http');
+var express = require('express');
+var util = require('util');
+var app = express();
+var server = http.createServer(app);
+var passport = require('passport');
+var auth = require('./auth');
+var views = require('./views');
+var api = require('./api');
+
+var serverPort = 8080;
+var apiClient = require('./apiclient');
+
+app.configure(function () {
+  app.use(express.logger());
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+  
+  app.set('views', __dirname + '/views')
+  app.set('view engine', 'jade');
+  app.use(express.static(__dirname + '/public'));
+  
+  app.use(express.errorHandler({ 
+    dumpExceptions: true, 
+    showStack: true 
+  }));
+
+  /**
+   * Root view (login view or files list depending whether user is logged in or not)
+   */
+  app.get('/', views.root); 
+  
+  /**
+   * New file dialog
+   */
+  app.get('/newfiledialog', views.newFileDialog);
+  
+  /**
+   * Setup view
+   */
+  app.get('/setup', views.setup);
+  
+  app.post('/setup', views.saveSetup);
+
+  /* Auth */
+  
+  // Facebook
+  app.get('/auth/facebook',  passport.authenticate('facebook', { scope: ['email'] } ));
+  app.get('/auth/facebook/callback', passport.authenticate('facebook'), function(req, res) {
+    res.redirect('/');
+  });
+  
+  app.get('/auth/github', passport.authenticate('github', { scope: ['user:email'] } ));
+  app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), function(req, res) {
+    res.redirect('/');
+  });  
+
+  /**
+   * Logs user out
+   */
+  app.get('/auth/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
+  
+  // Edit views
+  
+  app.get('/files/:fileid/edit/ckeditor', views.fileEditCKEditor);
+  
+  app.get('/files/:fileid/view', views.fileView);
+
+  
+  // API
+  
+  app.post('/files', api.fileCreate);
+  
+  /**
+    Client asks to join the collaboration of a document. 
+  **/
+  app.get('/files/:fileid/join', api.fileJoin);
+
+  /**
+   * Returns a file
+   */
+  app.get('/files/:fileid', api.fileGet);
+  
+  /**
+   * Saves a file
+   */
+  app.put('/files/:fileid', api.fileSave);
+
+  /**
+   * Patches a file
+   */
+  app.patch('/files/:fileid', api.filePatch);
+  
+});
+
+server.listen(serverPort);
---
+++ auth.js
@@ -0,0 +1,202 @@
+(function() {
+
+  var passport = require('passport');
+  var FacebookStrategy = require('passport-facebook').Strategy;
+  var GitHubStrategy = require('passport-github').Strategy;
+  var passport = require('passport');
+  var database = require('./database.js');
+  var apiClient = require('./apiclient');
+  var settings = require('./settings');
+  var rest = require('restler');
+  var _ = require('underscore');
+  
+  function resolveUser(identifier, emails, callback) {
+    database.model.UserIdentifier.findOne({ identifier: identifier }, function (uidErr, userIdentifier) {
+      if (uidErr) {
+        // Error occurred while trying to find UserIdentifier
+        callback(uidErr, null);
+      } else {
+        if (userIdentifier != null) {
+          // UserIdentifier found, trying to find matching User 
+          database.model.User.findOne({ _id: userIdentifier.userId }, function (userErr, user) {
+            callback(userErr, user);
+          });
+        } else {
+          database.model.UserEmail.find({ 'email': { $in: emails } }, function (emailErr, userEmails) {
+            if (userEmails.length == 0) {
+              // Could not find user, returning null
+              callback(null, null);
+            } else {
+              var firstUserId = userEmails[0].userId;
+              userEmails.forEach(function (userEmail) {
+                if (userEmail.userId != firstUserId) {
+                  callback("Several users found with given set of emails", null);
+                  return;
+                }
+              });
+              
+              database.model.User.findOne({ _id: firstUserId }, function (userErr, user) {
+                callback(userErr, user);
+              });
+            }
+          });
+        }
+      }
+    });
+  };
+  
+  function newUser(identifier, name, emails, callback) {
+    new database.model.User({ name: name }).save(function (err, newUser) {
+      if (err) {
+        return callback(err, null);
+      } else {
+        new database.model.UserIdentifier({
+          userId: newUser._id,
+          identifier: identifier
+        }).save(function (err, userIdentifier) {
+          callback(null, newUser);
+        });
+      }
+    });
+  };
+  
+  function addUserEmails(user, emails, callback) {
+    var result = new Array();
+    database.model.UserEmail.find({ 'email': { $in: emails } }, function (err, userEmails) {
+      var existingEmails = _.pluck(userEmails, 'email');
+      var missingEmails = existingEmails;
+      existingEmails.forEach(function (existingEmail) {
+        missingEmails = _.without(emails, existingEmail);
+      });
+      
+      if (missingEmails.length == 0) {
+        callback(null);
+      } else { 
+        missingEmails.forEach(function (email) {
+          new database.model.UserEmail({ userId: user._id, email: email }).save(function (err2, userMail) {
+	        result.push(err2);        
+		  
+		    if (result.length == missingEmails.length) {
+		      callback(result);
+		    }
+          }); 
+        });
+      }
+    });
+  }
+  
+  function loginUser(identifier, emails, name, callback) {
+    resolveUser(identifier, emails, function (err, user) {
+      if (err) {
+        return callback(err, null);
+      } else {
+        if (user == null) {
+          newUser(identifier, name, emails, function (err, user) {
+            addUserEmails(user, emails, function (errs) {
+              callback(err, user);
+            });
+          });
+        } else {
+          addUserEmails(user, emails, function (errs) {
+            return callback(null, user);
+          });
+        }
+      }
+    });
+  }
+  
+  function loginCoOps(user, callback) {
+    if (!user.accessToken) {
+      apiClient.get(function (client) {
+        client.createUser(user.name).on('complete', function(data, response) {
+          if (response.statusCode >= 200 && response.statusCode <= 299) {
+            var resp = data['response'];
+            var tokenResponse = resp['access_token'];
+          
+            user.userId = resp['user_id'];
+            user.accessToken = tokenResponse['access_token'];
+            user.refreshToken = tokenResponse['refresh_token'];
+            user.tokenExpires = new Date().getTime() + tokenResponse['expires_in'];
+          
+            user.save(function (err, user) {
+              callback(err);
+            });
+          } else {
+            callback("Could not connect to Co-Ops Server.");
+          }
+        });
+      });
+    } else {
+      callback(null);
+    }
+  }
+  
+  passport.serializeUser(function(user, done) {
+    done(null, user._id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    database.model.User.findOne({ _id: id }, function (userErr, user) {
+      done(userErr, user);
+    });
+  });
+  
+  settings.get(['facebook-client-id', 'facebook-client-secret'], function (err, settings) {
+    var settingMap = _.object(_.pluck(settings, 'key'), _.pluck(settings, 'value'));
+  
+    passport.use(new FacebookStrategy({
+        clientID: settingMap['facebook-client-id'],
+        clientSecret: settingMap['facebook-client-secret'],
+        callbackURL: "/auth/facebook/callback"
+      },
+      function(accessToken, refreshToken, profile, done) {
+        process.nextTick(function(){
+          var identifier = 'facebook-' + profile.id;
+          var emails = _.pluck(profile.emails, 'value');
+
+          loginUser(identifier, emails, profile.name, function (err, user) {
+            loginCoOps(user, function (err) {
+              done(err, user);              
+            });
+          });
+        });
+      }
+    ));
+  });
+  
+  settings.get(['github-client-id', 'github-client-secret'], function (err, settings) {
+    var settingMap = _.object(_.pluck(settings, 'key'), _.pluck(settings, 'value'));
+  
+    passport.use(new GitHubStrategy({
+        clientID: settingMap['github-client-id'],
+        clientSecret: settingMap['github-client-secret'],
+        callbackURL: "/auth/github/callback"
+      },
+      function(accessToken, refreshToken, profile, done) {
+        process.nextTick(function(){
+          var identifier = 'github-' + profile.id;
+          var emails = _.pluck(profile.emails, 'value');
+
+          rest.get('https://api.github.com/user/emails', {
+            headers: {
+              'Accept': 'application/vnd.github.v3',
+              'Authorization': 'token ' + accessToken
+            }
+          }).on('complete', function(result) {
+  			if (result instanceof Error) {
+  			  done(result, null);
+  			} else {
+  			  var emails = _.pluck(result, 'email');
+  			  loginUser(identifier, emails, profile.name, function (err, user) {
+	            loginCoOps(user, function (err) {
+	              done(err, user);              
+	            });
+	          });
+  			}
+	      });
+        });
+      }
+    ));
+  });
+  
+}).call(this);
