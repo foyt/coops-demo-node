e19b3a63372396d8adcff8a3242d404603dd922b
Antti Leppä <antti.leppa@foyt.fi>
2013-05-19 21:35:15 +0300

    ** Renamed ckeditor-init.js into edit-ckeditor.js

---
+++ public/edit-ckeditor.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  var location = window.document.location;
+  var path = location.pathname.split('/');
+  var fileId = path[2];
+  var serverUrl = location.protocol + '//' + location.host + '/' + path[1] + '/' + fileId + '';
+
+  CKEDITOR.plugins.addExternal('change', '/ckplugins/change/');
+  CKEDITOR.plugins.addExternal('coops', '/ckplugins/coops/');
+  CKEDITOR.plugins.addExternal('coops-dmp', '/ckplugins/coops-dmp/');
+  CKEDITOR.plugins.addExternal('coops-ws', '/ckplugins/coops-ws/');
+  CKEDITOR.plugins.addExternal('mrmonkey', '/ckplugins/mrmonkey/');
+  
+  var editor = CKEDITOR.appendTo( 'ckcontainer', { 
+    skin: 'moono',
+    extraPlugins: 'coops-ws,coops-dmp,coops,mrmonkey',
+    coops: {
+      serverUrl: serverUrl,
+      websocket: {
+        cursorsVisible: true,
+        cursorAlpha: 0.9,
+        cursorBlinks: true,
+        cursorBlinkInterval: 1.2
+      }
+    },
+    mrmonkey: {
+      typeRandom: true,
+      // typeRandomInterval: 
+      // typeRandomKeyCodes: null
+      // typeRandomMaxKeyStrokesAtOnce: 10
+      // typeRandomMaxKeyStrokeInterval: 10
+      
+      pasteLoremIpsum: false,
+      // pasteLoremIpsumText
+      // pasteLoremIpsumInterval:
+      pasteLoremIpsumBlockSize: 10,
+      
+      selectionChange: false,
+      selectionChangeInterval: 10000
+      
+    }
+  }, 'Content loading...');
+  
+});
